0ea0c63fa454b9b8f2bfc49c9c0ef2682daa588f
Furqon Aji Yudhistira <furqonajiy@gmail.com>
2025-09-11 13:04:12 +0200

    Initialize header component

--- fe-angular/src/app/component/header/header.component.ts
+++ fe-angular/src/app/component/header/header.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TopNavComponent } from '../top-nav/top-nav.component';
+import { SecondNavComponent } from '../second-nav/second-nav.component';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [TopNavComponent, SecondNavComponent],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
