a47adc85e6cb36fd9cf9ec6a56009d8ae54ddf17
Furqon Aji <furqon.aji.y@accenture.com>
2025-09-12 00:03:43 +0700

    Add RouterLink for Client

--- fe-angular/src/app/app.routes.ts
+++ fe-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClientComponent } from './components/client/client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'client',
+        component: ClientComponent
+    }
+];
--- fe-angular/src/app/components/header/second-nav/second-nav.component.ts
+++ fe-angular/src/app/components/header/second-nav/second-nav.component.ts
@@ -3,20 +3,21 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-second-nav',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatFormField, MatInputModule, MatButtonModule, FormsModule],
+  imports: [CommonModule, MatIconModule, MatFormField, MatInputModule, MatButtonModule, FormsModule, RouterLink],
   templateUrl: './second-nav.component.html',
   styleUrl: './second-nav.component.css'
 })
 export class SecondNavComponent {
-  q ='';
+  q = '';
 
-    onSearch() {
+  onSearch() {
     // hook up later
     console.log('Search:', this.q);
   }
